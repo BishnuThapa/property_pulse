decbfe10394b94d3dca0c3990ce03d7db785be54
Bishnu <thapabishnu20@gmail.com>
2025-01-06 18:50:36 +0545

    fetch data from server components & randomly display to homepage

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -7,8 +7,12 @@ import PropertyCard from "@/components/PropertyCard";
 async function fetchProperties() {
   try {
     const res = await fetch(`${process.env.NEXT_PUBLIC_API_DOMAIN}/properties`);
-    
+    if (!res.ok) {
+      throw new Error('Faild to fetch data.')
+    }
+    return res.json()
   } catch (error) {
+    console.log(error);
     
   }
 }
--- components/HomeProperties.jsx
+++ components/HomeProperties.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import Link from "next/link";
-import properties from "@/properties.json";
+// import properties from "@/properties.json";
 import PropertyCard from "@/components/PropertyCard";
 
 
-const HomeProperties = () => {
-  const recentProperties = properties
-    .sort(() => Math.random() - Math.random())
-    .slice(0, 3);
-  return (
+async function fetchProperties() {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_DOMAIN}/properties`);
+    if (!res.ok) {
+      throw new Error("Faild to fetch data.");
+    }
+    return res.json();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const HomeProperties = async() => {
+  const properties= await fetchProperties()
+  const recentProperties = properties.sort(() => Math.random() - Math.random()).slice(0, 3);
+  return ( 
     // <!-- Recent Properties -->
     <>
       <section className="px-4 py-6">
